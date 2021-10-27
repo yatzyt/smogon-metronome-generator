6b3eba7e32a0b775dac211d7d2af2ba1eed6314d
Andrew <antongyang@yahoo.com>
2021-10-27 12:37:35 -0600

    added a space between pokemon name and dex number

--- src/client/pokemon.tsx
+++ src/client/pokemon.tsx
@@ -5,7 +5,7 @@ const PokemonDisplay = (poke: Pokemon) => (
     <div className={tw`flex flex-col justify-self-center justify-center items-center`}>
         <img src={poke.species.imageUrl} width={250} height={250}/>
         <h3>
-            {poke.species.name}
+            {poke.species.name + ' '}
             <small>{poke.species.pokedexNum}</small>
         </h3>
         <p>Ability: {poke.ability.name}</p>
